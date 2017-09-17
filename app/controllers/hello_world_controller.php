6c926e04972da9c383c8b0bd4fd8b33c3777629a
Henri Niskanen <henri.niskanen@helsinki.fi>
2017-09-17 23:10:27 +0300

    suunnittelu sivut

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -9,6 +9,43 @@
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      //echo 'Hello World!';
+      View::make('helloworld.html');
+    }
+
+    public static function game_list(){
+      View::make('suunnitelmat/game_list.html');
+    }
+
+    public static function game_show(){
+      View::make('suunnitelmat/game_show.html');
+    }
+
+    public static function login(){
+      View::make('suunnitelmat/login.html');
+    }
+
+    public static function register(){
+      View::make('suunnitelmat/register.html');
+    }
+
+    public static function profiili(){
+      View::make('suunnitelmat/profile.html');
+    }
+
+    public static function esine_lista(){
+      View::make('suunnitelmat/auctionList.html');
+    }
+
+    public static function muokkaa_profiili(){
+      View::make('suunnitelmat/editProfile.html');
+    }
+
+    public static function nayta_esine(){
+      View::make('suunnitelmat/auction.html');
+    }
+
+    public static function lisaa_esine(){
+      View::make('suunnitelmat/addAuction.html');
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,33 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+
+  $routes->get('/game', function() {
+    HelloWorldController::game_list();
+  });
+  $routes->get('/game/1', function() {
+    HelloWorldController::game_show();
+  });
+
+  $routes->get('/login', function() {
+    HelloWorldController::login();
+  });
+  $routes->get('/register', function() {
+    HelloWorldController::register();
+  });
+  $routes->get('/esine', function() {
+    HelloWorldController::esine_lista();
+  });
+  $routes->get('/esine/1', function() {
+    HelloWorldController::nayta_esine();
+  });
+  $routes->get('/lisaaesine', function() {
+    HelloWorldController::lisaa_esine();
+  });
+  $routes->get('/profiili', function() {
+    HelloWorldController::profiili();
+  });
+  $routes->get('/muokkaa-profiili', function() {
+    HelloWorldController::muokkaa_profiili();
+  });
+
